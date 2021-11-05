3c31c3237501763f115d2cffdadb8732cbd27db4
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-05 15:21:35 -0500

    creando servicio para loguear al usuario

---
+++ part1-react/src/services/login.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+const baseUrl='http://localhost:3001/api/login'
+
+const login = async credential =>{
+  const {data} = await axios.post(baseUrl, credential)
+  return data
+}
+
+export default  { login } 
