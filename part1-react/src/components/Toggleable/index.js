d080626634b44f6b4b3732cf52cb3d7dd104130d
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-09 15:26:20 -0500

    creando componente toggleable para poder ocultar elementos y mostrarlos

---
+++ part1-react/src/components/Toggleable/index.js
@@ -0,0 +1,20 @@
+import {useState} from 'react'
+
+export default function Toggleable ({children, buttonLabel}){
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : ''}
+  const showWhenVisible = { display: visible ? '' : 'none'}
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={()=>setVisible(true)}>{buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {children}
+        <button onClick={()=>setVisible(false)}>Cancel</button>
+      </div>
+    </div>
+    )
+}
