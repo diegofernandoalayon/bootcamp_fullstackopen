b55cccfe471ce0c01ab4e3ab057303978615718c
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-09 15:37:48 -0500

    envolviendo el formulario de login con el componente toggleable

--- part1-react/src/components/FormLogin/index.js
+++ part1-react/src/components/FormLogin/index.js
@@ -1,29 +1,38 @@
+// import { useState } from "react"
+
+import Toggleable from "../Toggleable";
 
 export default function FormLogin({username, password, handleLoginSubmit, ...props}){
+ 
 
   return(
-    <form onSubmit={handleLoginSubmit}>
-    <div>
-      <input
-        type='text'
-        value={username}
-        name='Username'
-        placeholder='Username'
-        onChange={props.handleUsernameChange}
-        />
-    </div>
-    <div>
-      <input
-        type='password'
-        value={password}
-        name='password'
-        placeholder='Password'
-        onChange={props.handlePasswordChange}
-        />
-    </div>
-      <button>
-        Login
-      </button>
-  </form>
+    <Toggleable buttonLabel={'Show login'}>
+      <form onSubmit={handleLoginSubmit}>
+        <div>
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            placeholder='Username'
+            onChange={props.handleUsernameChange}
+            />
+        </div>
+        <div>
+          <input
+            type='password'
+            value={password}
+            name='password'
+            placeholder='Password'
+            onChange={props.handlePasswordChange}
+            />
+        </div>
+        <button>
+          Login
+        </button>
+      </form>
+    </Toggleable>
+      
+   
+    
   )
 }
