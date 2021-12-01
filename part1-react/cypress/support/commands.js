6c35606afd08687b4bb18cc66ba127f6e45bcdc4
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-01 13:12:25 -0500

    creando un comando de cypress para agregar notas

--- part1-react/cypress/support/commands.js
+++ part1-react/cypress/support/commands.js
@@ -10,3 +10,15 @@ Cypress.Commands.add('login', ({ username, password }) => {
     cy.visit('http://localhost:3000')
   })
 })
+
+Cypress.Commands.add('createNote', ({ content, important }) => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:3001/api/notes',
+    body: { content, important },
+    headers: {
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem('loggedNoteAppUser')).token}`
+    }
+  })
+  cy.visit('http://localhost:3000')
+})
