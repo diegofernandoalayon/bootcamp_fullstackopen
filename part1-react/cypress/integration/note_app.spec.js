f2868a6af085ac53df82a423b46f710860ac6077
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-30 23:46:27 -0500

    creando test para agregar una nota

--- part1-react/cypress/integration/note_app.spec.js
+++ part1-react/cypress/integration/note_app.spec.js
@@ -1,11 +1,35 @@
 
 describe('Note App', () => {
-  it('frontpage can be opened', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
+  })
+
+  it('frontpage can be opened', () => {
     cy.contains('Notes')
   })
   it('login form can be opened', () => {
-    cy.visit('http://localhost:3000')
     cy.contains('Show login').click()
+  })
+  it('user can login', () => {
+    cy.contains('Show login').click()
+    cy.get('[placeholder=Username]').type('dfar')
+    cy.get('[placeholder=Password]').type('dfar98')
+    cy.get('#form-login-button').click()
+    cy.contains('Create a new note')
+  })
+  describe('when logged in', () => {
+    beforeEach(() => {
+      cy.contains('Show login').click()
+      cy.get('[placeholder=Username]').type('dfar')
+      cy.get('[placeholder=Password]').type('dfar98')
+      cy.get('#form-login-button').click()
+      cy.contains('Create a new note')
+    })
+    it('a new note can be created', () => {
+      cy.contains('New Note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('Crear nota').click()
+      cy.contains('a note created by cypress')
+    })
   })
 })
--- part1-react/src/components/FormLogin/index.js
+++ part1-react/src/components/FormLogin/index.js
@@ -24,7 +24,7 @@ export default function FormLogin ({ username, password, handleLoginSubmit, ...p
             onChange={props.handlePasswordChange}
           />
         </div>
-        <button>
+        <button id='form-login-button'>
           Login
         </button>
       </form>
